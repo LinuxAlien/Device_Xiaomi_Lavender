9e25d93f6cc49d2b7f2ece3cabffd846fc1a6aba
LinuxAlien <murik8900@gmail.com>
2021-01-22 10:43:48 +0000

    lavender: Bringup legion [2/1]

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_lavender.mk
+    $(LOCAL_DIR)/legion_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_lavender-eng \
-    evolution_lavender-userdebug \
-    evolution_lavender-user
+    legion_lavender-eng \
+    legion_lavender-userdebug \
+    legion_lavender-user
